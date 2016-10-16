a5ae4e7d0d6b1d987e8c7bc8be5959ad89da8ab8
theandersonn <theandersonn@gmail.com>
2016-10-16 18:03:44 -0200

    Added cpt portfolio

---
+++ custom-post-types.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Custom Post Types
+Plugin URI: https://github.com/theandersonn/custom-post-types
+Description: Modelo para crição de CPTs
+Author: Anderson Nascimento
+Version: 0.1
+Author URI: http://theandersonn.com/
+*/
+
+require_once( plugin_dir_path( __FILE__ ) . '/cpts/portfolio.php' );
